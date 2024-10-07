c418def5dc77d1aa1bfe35b058f3be733f09197c
Stanislav Povolotsky <stas.github@povolotsky.info>
2024-10-07 19:57:32 +0200

    Updated start scripts: added 'update' action support

--- jddlab.cmd
+++ jddlab.cmd
@@ -3,6 +3,12 @@ setlocal enabledelayedexpansion
 set folder_to_serve="%CD%"
 set jddlab_docker_image=stanislavpovolotsky/jddlab:latest
 
+if "%~1"=="update" (
+  echo Updating jddlab...
+  docker pull "%jddlab_docker_image%"
+  exit /b %ERRORLEVEL%
+)
+
 set "args="
 :loop
   if "%~1"=="" ( if "%~2"=="" ( if "%~3"=="" ( goto execute ) ) )
@@ -12,3 +18,4 @@ goto loop
 
 :execute
 docker run -it --rm -v "%folder_to_serve%:/work" "%jddlab_docker_image%" %args%
+exit /b %ERRORLEVEL%
