735bc8f0c5391003ec4908245ee9ce8a75ff7e0e
Stanislav Povolotsky <stas.github@povolotsky.info>
2025-06-17 00:58:02 +0200

    Switch to maven 3.9.10

--- scripts/installation/prepare.sh
+++ scripts/installation/prepare.sh
@@ -3,9 +3,9 @@ set -e
 
 apt-get update && apt-get install -y curl ca-certificates python3-pip python3-venv zip openjdk-21-jdk-headless git jq xmlstarlet python3-distutils
 
-# Installing mvn
+echo Installing mvn
 pushd /tmp
-curl -O https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+curl -O https://dlcdn.apache.org/maven/maven-3/3.9.10/binaries/apache-maven-3.9.10-bin.tar.gz
 tar -xvf apache-maven-*.tar.gz
 rm -f apache-maven-*.tar.gz
 mv apache-maven-* /opt/
@@ -13,5 +13,5 @@ ln -s /opt/apache-maven-*/bin/mvn /usr/local/bin/mvn
 popd
 mvn dependency:get >/dev/null 2>&1 || echo MVN precached
 
-# Installing rust compiler
+echo Installing rust compiler
 curl https://sh.rustup.rs -sSf | sh -s -- -y
