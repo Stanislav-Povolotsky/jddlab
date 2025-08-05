e5095b168c13f180737008f4e6257e6a4c9d3f7c
Stanislav Povolotsky <stas.github@povolotsky.info>
2025-08-05 11:38:35 +0200

    Using latest maven version

--- scripts/installation/prepare.sh
+++ scripts/installation/prepare.sh
@@ -5,7 +5,12 @@ apt-get update && apt-get install -y curl ca-certificates python3-pip python3-ve
 
 echo Installing mvn
 pushd /tmp
-curl -O https://dlcdn.apache.org/maven/maven-3/3.9.10/binaries/apache-maven-3.9.10-bin.tar.gz
+#curl -O https://dlcdn.apache.org/maven/maven-3/3.9.10/binaries/apache-maven-3.9.10-bin.tar.gz
+curl -o maven-index.html https://dlcdn.apache.org/maven/maven-3/
+maven_latest_version=$(grep -oP '(?<=<a href=")[0-9]+\.[0-9]+\.[0-9]+/' maven-index.html | sed 's:/$::' | sort -V | tail -n1 | tr -d '\n' | tr -d '\r')
+rm maven-index.html
+echo "Detected latest maven version: $maven_latest_version"
+curl -O https://dlcdn.apache.org/maven/maven-3/$maven_latest_version/binaries/apache-maven-$maven_latest_version-bin.tar.gz
 tar -xvf apache-maven-*.tar.gz
 rm -f apache-maven-*.tar.gz
 mv apache-maven-* /opt/
